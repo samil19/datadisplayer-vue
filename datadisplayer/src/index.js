67cdff5dc370f3eec3f7f2e1f061f6347cc8531a
Samil Francisco <samil.ibm@outlook.com>
2020-03-22 00:33:22 -0300

    Se agrego datadisplayer como un componente

--- datadisplayer/src/index.js
+++ datadisplayer/src/index.js
@@ -1,7 +1,7 @@
-import datadisplayervue from "./App.vue";
+import App from "./App.vue";
 
 export default {
  install(Vue, options) {
-  Vue.component("datadisplayer-vue", datadisplayervue);
+  Vue.component("datadisplayer-vue", App);
  }
 };
--- datadisplayer/src/main.js
+++ datadisplayer/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import datadisplayervue from './App.vue'
+import App from './App.vue'
 import { CardPlugin,PaginationPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -10,5 +10,5 @@ Vue.use(PaginationPlugin)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(datadisplayervue),
+  render: h => h(App),
 }).$mount('#app')
